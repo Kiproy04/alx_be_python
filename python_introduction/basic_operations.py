f6a04006b29959edd4e88d7ee5dd99c4c6bb177e
Leroy Kirui <delleroy04@gmail.com>
2025-05-20 19:39:47 +0300

    Wrote something

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,14 @@
+number1 = 10
+number2 = 5
+
+##sum of the two variables
+sum = number1 + number2
+print(sum)
+
+##product of the variables
+product = int(number1 * number2)
+print(product)
+
+##difference of the variables
+subtract = int(number2 - number1)
+print(subtract)
