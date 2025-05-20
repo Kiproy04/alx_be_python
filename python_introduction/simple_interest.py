c062114d26a0addf58117797f65dc806893c74c0
Leroy Kirui <delleroy04@gmail.com>
2025-05-20 20:25:34 +0300

    Calculate interest

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -2,6 +2,5 @@ principal = 1000
 rate = 0.05
 time = 3
 #calculate the interest
-(I(P * R * T)) # type: ignore
-interest = float(principal * rate * time)
-print("The simple interest is:", (interest))
+interest = (principal * rate * time)
+print("The simple interest is:", float(interest))
