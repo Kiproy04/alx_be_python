05ce9320cee71b4f9ce4cd0417119b41ce53a267
Leroy Kirui <delleroy04@gmail.com>
2025-05-25 23:00:16 +0300

    Fixed formula issue

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,6 @@
-formula = f"(I = P * R * T)"
 Principal = 1000
 Rate = 0.05
 Time = 3
 #calculate the interest, formula (I = P * R * T)
-interest = (Principal * Rate * Time)
+interest = Principal * Rate * Time
 print("The simple interest is:", float(interest))
