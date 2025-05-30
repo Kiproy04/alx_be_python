775f790d7adb714c0e4dead0034a4ca1a91e064a
Leroy Kirui <delleroy04@gmail.com>
2025-05-30 20:07:05 +0300

    Created a reminder program

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -5,18 +5,18 @@ Time_Bound = str(input("Is it time-bound? (yes/no): "))
 match Priority:
     case "high":
         if Time_Bound == "yes":
-            print(f"{Task} is a {Priority} priority task that requires immediate attention today! ")
+            print(f"Reminder: {Task} is a {Priority} priority task that requires immediate attention today! ")
         else:
-            print(f"{Task} is a {Priority} priority task consider completing it when you have free time.")
+            print(f"Note: {Task} is a {Priority} priority task consider completing it when you have free time.")
     case "medium":
         if Time_Bound == "yes":
-            print(f"{Task} is a {Priority} priority task that requires immediate attention today!")
+            print(f"Reminder: {Task} is a {Priority} priority task that requires immediate attention today!")
         else:
-            print(f"{Task} is a {Priority} priority task consider completing it when you have free time.")
+            print(f"Note: {Task} is a {Priority} priority task consider completing it when you have free time.")
     case "low":
         if Time_Bound == "yes":
-            print(f"{Task} is a {Priority} priority task that requires immediate attention today!")
+            print(f"Reminder: {Task} is a {Priority} priority task that requires immediate attention today!")
         else:
-            print(f"{Task} is a {Priority} priority task consider completing it when you have free time.")
+            print(f"Note: {Task} is a {Priority} priority task consider completing it when you have free time.")
     case _:
         print("Invaid input")
