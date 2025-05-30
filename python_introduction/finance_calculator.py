9bda30d1a1f9dd92a5e7beedb5c577d9b69da2da
Leroy Kirui <delleroy04@gmail.com>
2025-05-30 20:26:56 +0300

    Solved calculations

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
 ##Prompt the user for financial details
 monthly_income = float(input("Enter your monthly income: "))
-monthly_expense = float(input("Enter your total monthly expenses: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
 ##calculate monthly savings
-monthly_savings = (monthly_income - monthly_expense)
+monthly_savings = (monthly_income) - (monthly_expenses)
 print("Your total monthly savings is:", monthly_savings)
 
 
