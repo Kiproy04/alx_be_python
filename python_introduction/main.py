86a3bb3013c68e133ad5055e37774350dccf91ad
Leroy Kirui <delleroy04@gmail.com>
2025-06-05 13:53:58 +0300

    Working with modules

--- python_introduction/main.py
+++ python_introduction/main.py
@@ -1,5 +1,17 @@
+import os
+cwd = os.getcwd()
+import sys
+sys.path.append(cwd)
+
+from pathlib import Path
+path = Path(cwd)
+a=str(path.parent.absolute())
+
+sys.path.append(a)
+
+# Importing the basic_operations module from the python_introduction package
 from python_introduction import basic_operations
-basic_operations.add(5, 3)
-basic_operations.subtract(5, 3)
-basic_operations.multiply(5, 3)
-basic_operations.divide(5, 3)
+print(basic_operations.add(5, 3))
+print(basic_operations.subtract(5, 3))
+print(basic_operations.multiply(5, 3))
+print(basic_operations.divide(5, 3))
