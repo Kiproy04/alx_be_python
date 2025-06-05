1c98a162750f5a6ab95ef1188d525207f7561a5c
Leroy Kirui <delleroy04@gmail.com>
2025-06-05 11:56:43 +0300

    Practise exercise

---
+++ python_introduction/datastructures.py
@@ -0,0 +1,10 @@
+Fruits = ["apple", "banana", "apricot", "peach", "coconut"]
+print(Fruits[2])  # Output: apricot
+
+##Dict
+Books = {"author": "leroy", "title": "Python for Beginners", "year": 2023, "genre": "Programming"}
+print(Books["genre"])  # Output: Programming
+
+##Set
+me = set(range(11))
+print(me)
