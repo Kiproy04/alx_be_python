3dd2080ec0c2943b12bde012212464c4f3fcfc75
Leroy Kirui <delleroy04@gmail.com>
2025-06-06 14:28:40 +0300

    Played with datastructures

--- python_introduction/datastructures.py
+++ python_introduction/datastructures.py
@@ -1,10 +1,16 @@
 Fruits = ["apple", "banana", "apricot", "peach", "coconut"]
-print(Fruits[2])  # Output: apricot
+print(Fruits)  # Output: apricot
 
 ##Dict
 Books = {"author": "leroy", "title": "Python for Beginners", "year": 2023, "genre": "Programming"}
-print(Books["genre"])  # Output: Programming
+print(Books)  # Output: Programming
 
 ##Set
-me = set(range(11))
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 2, 3]
+me = set(list)
 print(me)
+
+##Tuple
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)  # Output: (1, 2, 3, 4, 5)
+
