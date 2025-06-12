f8b6d2e8505c703614ebcb193e04d8c2bdf312b9
Leroy Kirui <delleroy04@gmail.com>
2025-06-12 13:25:26 +0300

    Small banking system

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -13,7 +13,7 @@ class BankAccount:
     def withdraw(self, amount):
         if amount > 0:
             if self.__account_balance < amount:
-                print("Insufficient funds")
+                return "Insufficient funds."
             else:
                 self.__account_balance -= amount
                 return self.__account_balance
