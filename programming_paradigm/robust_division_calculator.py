9a8edd45e1aa856886ac97c07c10fe37ab7fef70
Leroy Kirui <delleroy04@gmail.com>
2025-06-13 08:43:32 +0300

    Handling division by zero error

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,4 +5,4 @@ def safe_divide(float(numerator), float(denominator)):
         else:
             return numerator / denominator
     except ValueError:
-        print("Error: Please enter numeric values only")
+        print("Error: Please enter numeric values only.")
