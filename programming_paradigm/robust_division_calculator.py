48083660d95102e9ee1daa17ce28c43d2ca9f1a4
Leroy Kirui <delleroy04@gmail.com>
2025-06-14 08:49:53 +0300

    Handling division by zero error

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,5 +1,7 @@
-def safe_divide(float(numerator), float(denominator)):
+def safe_divide(numerator, denominator):
     try:
+        numerator = float(numerator)
+        denominator = float(denominator)
         if denominator <= 0:
             raise ZeroDivisionError("Error: Cannot divide by zero.")
         else:
