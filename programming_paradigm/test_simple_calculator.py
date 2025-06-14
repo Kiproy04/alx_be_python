030416b97baecdd011cab1a82909b9a69f3e6f8f
Leroy Kirui <delleroy04@gmail.com>
2025-06-14 15:39:14 +0300

    Learning how to test my code

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,5 +12,16 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
 
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_subtraction(self):
+        """Test the subtraction method."""
+        self.assertEqual(self.calc.subtract(2, 3), 5)
+        self.assertEqual(self.calc.subtract(-1, 1), 0)
+
+    
+
 if __name__ == "__main__":
     unittest.main()
