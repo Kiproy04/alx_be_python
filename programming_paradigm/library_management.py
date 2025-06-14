4183ebcefde3b3ee6078b41c65dc861e12109c19
Leroy Kirui <delleroy04@gmail.com>
2025-06-14 16:37:34 +0300

    Mini library

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,15 @@
+class Book:
+    def __init__(self, title, author, _is_checked_out):
+        self.title = title
+        self.author = author
+        self._is_checked_out = _is_checked_out
+    class Library:
+        def __init__( self):
+            _books = Book[]
+        def add_book(self):
+            book_name = self.title
+            return book_name
+        def check_out_book(self, title):
+
+        def return_book(self, title):
+        def list_available_books(self):
