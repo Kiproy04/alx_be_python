ff8f40ff860eca57a9b0d39a698d27e5064acf51
Leroy Kirui <delleroy04@gmail.com>
2025-06-19 21:02:48 +0300

    First try

--- oop/book_class.py
+++ oop/book_class.py
@@ -5,10 +5,10 @@ class Book:
         self.year = int(year)
         
     def __str__(self):
-        return "(title) by (author), published in (year)"
+        return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self):
-        return f"Book('{self.title}', '{self.author}', {self.year})"
+        return f"Book ('{self.title}', '{self.author}', {self.year})"
 
     def __del__(self):
-        print(f"Deleting ({self.title} of the book)")
+        print(f"Deleting ({self.title}")
