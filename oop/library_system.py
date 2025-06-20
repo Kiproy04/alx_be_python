8add45560b621829228553a526cb7988fda65265
Leroy Kirui <delleroy04@gmail.com>
2025-06-20 16:10:52 +0300

    Library system

--- oop/library_system.py
+++ oop/library_system.py
@@ -6,47 +6,33 @@ class Book:
     def __str__(self):
         return f"Book: {self.title} by {self.author}"
          
-    
-    def add_book(self):
-        self.title.author.add_book()
-
-    def list_books(self):
-        self.title.author.list_books()
-
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = int(file_size)
 
-        def __str__(self):
-            return f"EBook: {self.title} by {self.author} File Size: {self.file_size}"
+    def __str__(self):
+        return f"EBook: {self.title} by {self.author} File Size: {self.file_size}"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = int(page_count)
 
-        def __str__(self):
-            return f"PrintBook: {self.title} by {self.author} Page Count: {self.page_count}"
+    def __str__(self):
+        return f"PrintBook: {self.title} by {self.author} Page Count: {self.page_count}"
 
 
 class Library:
-    def __init__(self, books=None):
+    def __init__(self):
         self.books = []
 
     def add_book(self, book):
-        if isinstance(book, Book):
             self.books.append(book)
-        else:
-            print("Only Book instances can be added.")
-
 
     def list_books(self):
-        if not self.books:
-            print("No books available.")
-            return
         for book in self.books:
-            return f"- {book}"
+            print(book)
         
 
 
